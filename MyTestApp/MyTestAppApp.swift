8e9c76982267b4e9b87e29b0e9860cce54c43add
Carles Garcia <asdeoz@gmail.com>
2021-03-06 09:20:01 -0700

    Initial Commit

---
+++ MyTestApp/MyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTestAppApp.swift
+//  MyTestApp
+//
+//  Created by Carles Garcia Bercial on 3/6/21.
+//
+
+import SwiftUI
+
+@main
+struct MyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
